77d7c52471c8bc17d4512b04f53f04ac8c64f951
Ismam Ahnaf Mahin <ismam.mahin@northsouth.edu>
2025-10-06 14:56:13 +0600

    templates files added and configured

--- online_library/settings.py
+++ online_library/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'online_library.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
